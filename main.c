cd22d8e080ebcdc797ac9bb49c36869701e125b8
jgensler8 <5685042+jgensler8@users.noreply.github.com>
2025-06-02 15:34:28 -0500

    add menu selection

--- main.c
+++ main.c
@@ -48,25 +48,127 @@ void draw_frame(uint8_t screen_x, uint8_t screen_y, uint8_t width, uint8_t heigh
     }
 }
 
-void clear_bkg()
+void clear_bkg(void)
 {
     // this is the space tile
     uint8_t tile = 0;
-    fill_bkg_rect(0,0, 31,31, tile);
+    fill_bkg_rect(0, 0, 31, 31, tile);
 }
 
-void draw_frame_text(char* line_1, char* line_2)
+void clear_bkg_frame(void)
+{
+    // this is the space tile
+    uint8_t tile = 0;
+    fill_bkg_rect(0, 14, 31, 4, tile);
+}
+
+void init_font_vram(void)
 {
     set_bkg_1bpp_data(0, 'Z' - ' ' + 1, lankygitmono_tiles);
+}
+
+void init_frame_vram(void)
+{
+    set_bkg_1bpp_data('Z' - ' ' + 1, 9, sframe7_tiles);
+}
+
+void draw_frame_text(char *line_1, char *line_2)
+{
+    clear_bkg_frame();
     xy_printf(1, 15, line_1);
     xy_printf(1, 16, line_2);
-    set_bkg_1bpp_data('Z' - ' ' + 1, 9, sframe7_tiles);
     draw_frame(0, 14, 20, 4);
     SHOW_BKG;
 }
 
+uint8_t press_wait = 0;
+void wait_press(uint8_t button)
+{
+    if (press_wait)
+    {
+        while (joypad() & button)
+        {
+        }
+        press_wait = 0;
+    }
+    while (!(joypad() & button))
+    {
+        // do nothing
+        press_wait = 1;
+    }
+}
+
+uint8_t prompt_menu_2(char *o1, char *o2)
+{
+    uint8_t menu_selection = 0;
+    uint8_t last_render_menu_selection = 1;
+    uint8_t min_option = 0;
+    uint8_t max_option = 1;
+    clear_bkg_frame();
+    while (joypad())
+    {
+    }
+    uint8_t drew_frame = 0;
+    while (1)
+    {
+        // handle input
+        if (joypad() & J_DOWN)
+        {
+            menu_selection += 1;
+            menu_selection = menu_selection > max_option ? max_option : menu_selection;
+        }
+        else if (joypad() & J_UP)
+        {
+            menu_selection -= 1;
+            // wrapped
+            menu_selection = menu_selection > max_option + 1 ? min_option : menu_selection;
+        }
+        if (joypad() & J_A)
+        {
+            break;
+        }
+        // render frame
+        if (!drew_frame)
+        {
+            draw_frame_text(o1, o2);
+            drew_frame = 1;
+        }
+        // render cursor
+        if (menu_selection != last_render_menu_selection)
+        {
+            for (uint8_t possible_sel = 0; possible_sel <= max_option; possible_sel++)
+            {
+                if (possible_sel == menu_selection)
+                {
+                    set_bkg_tile_xy(0, 15 + possible_sel, 'X' - ' ');
+                }
+                else
+                {
+                    set_bkg_tile_xy(0, 15 + possible_sel, 0);
+                }
+            }
+            last_render_menu_selection = menu_selection;
+        }
+    }
+    return menu_selection;
+}
+
+#define EMPTY "\0"
 void main(void)
 {
+    init_font_vram();
+    init_frame_vram();
+
     clear_bkg();
     draw_frame_text("HELLO!", "THERE...");
+    wait_press(J_A);
+    switch (prompt_menu_2("JUMP", "RUN"))
+    {
+    case 0:
+        draw_frame_text("WISE!", EMPTY);
+        break;
+    case 1:
+        draw_frame_text("BAD!", EMPTY);
+        break;
+    }
 }
